43499534b293ab6f5cd30b9eb824a904dd7968ea
elon-kim <eunjong147@pusan.ac.kr>
2021-07-14 22:34:02 +0900

    ADD basic function

--- hand_detect.py
+++ hand_detect.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import time
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
@@ -20,9 +22,13 @@ with mp_hands.Hands(min_detection_confidence=0.5,
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+            two_hands = results.multi_handedness
+            for one_hand, hand_landmarks in zip(two_hands, results.multi_hand_landmarks):
+                print(one_hand.classification[0].label, end=" // ")
+                print(hand_landmarks.landmark[9].x)
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            print()
         cv2.imshow('MediaPipe Hands', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
